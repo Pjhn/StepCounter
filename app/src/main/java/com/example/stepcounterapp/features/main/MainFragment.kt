687c470a1d67d5cf2a979f5c5027d0d4b736fec8
Jeonghoon Park <hoondb@naver.com>
2025-01-31 20:52:03 +0900

    feat: 메인화면, 운동정보 프레그먼트 추가

---
+++ app/src/main/java/com/example/stepcounterapp/features/main/MainFragment.kt
@@ -0,0 +1,28 @@
+package com.example.stepcounterapp.features.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import com.example.stepcounterapp.features.main.presentation.screen.MainScreen
+import com.example.stepcounterapp.ui.theme.StepCounterAppTheme
+
+class MainFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                StepCounterAppTheme {
+                    Text("MainFragment")
+                    //MainScreen()
+                }
+            }
+        }
+    }
+}
