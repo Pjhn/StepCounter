d67b682b343d2791dab8600a611cb90b742c67e0
Jeonghoon Park <hoondb@naver.com>
2025-02-01 23:45:35 +0900

    feat: 메인화면 fragment 수정
    - viewModel의 사용자 데이터 구독

--- app/src/main/java/com/example/stepcounterapp/features/main/MainFragment.kt
+++ app/src/main/java/com/example/stepcounterapp/features/main/MainFragment.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.material3.Text
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.stepcounterapp.features.main.presentation.screen.MainScreen
+import com.example.stepcounterapp.features.main.presentation.viewmodel.MainViewModel
 import com.example.stepcounterapp.ui.theme.StepCounterAppTheme
 
 class MainFragment: Fragment() {
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -19,7 +23,9 @@ class MainFragment: Fragment() {
         return ComposeView(requireContext()).apply {
             setContent {
                 StepCounterAppTheme {
-                    MainScreen()
+                    MainScreen(
+                        viewModel.userRecord.collectAsState()
+                    )
                 }
             }
         }
