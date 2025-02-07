3ce8babe83cafaf1fa76e42f9fe130dee8185742
Jeonghoon Park <hoondb@naver.com>
2025-02-08 08:02:20 +0900

    feat: 권한 요청 기능 추가

--- app/src/main/java/com/example/stepcounterapp/features/main/MainFragment.kt
+++ app/src/main/java/com/example/stepcounterapp/features/main/MainFragment.kt
@@ -1,11 +1,17 @@
 package com.example.stepcounterapp.features.main
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.platform.ComposeView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -14,6 +20,7 @@ import androidx.lifecycle.repeatOnLifecycle
 import com.example.stepcounterapp.features.main.presentation.output.MainUiEffect
 import com.example.stepcounterapp.features.main.presentation.screen.MainScreen
 import com.example.stepcounterapp.features.main.presentation.viewmodel.MainViewModel
+import com.example.stepcounterapp.features.service.MeasurementService
 import com.example.stepcounterapp.ui.theme.StepCounterAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
@@ -23,12 +30,17 @@ import kotlinx.coroutines.launch
 class MainFragment : Fragment() {
     private val viewModel: MainViewModel by viewModels()
 
+    private val permissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) {}
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         observeUiEffects()
+        requestPermissions()
 
         return ComposeView(requireContext()).apply {
             setContent {
@@ -49,15 +61,17 @@ class MainFragment : Fragment() {
                 viewModel.output.mainUiEffect.collectLatest {
                     when (it) {
                         is MainUiEffect.StartMeasurement -> {
-                            /**
-                             * 백그라운드 측정 서비스 로직 동작
-                             * */
+                            val intent = Intent(requireContext(), MeasurementService::class.java)
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                                requireContext().startForegroundService(intent)
+                            }else{
+                                requireContext().startService(intent)
+                            }
                         }
 
                         is MainUiEffect.PauseMeasurement -> {
-                            /**
-                             * 백그라운드 측정 서비스 로직 중단
-                             * */
+                            val intent = Intent(requireContext(), MeasurementService::class.java)
+                            requireContext().stopService(intent)
                         }
 
                         is MainUiEffect.OpenSensitivityDialog -> {
@@ -68,6 +82,37 @@ class MainFragment : Fragment() {
                     }
                 }
             }
+        }
+    }
+
+    private fun requestPermissions() {
+        val permissionsToRequest = mutableListOf<String>()
+
+        // ACTIVITY_RECOGNITION 권한 (Android 10 이상)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACTIVITY_RECOGNITION
+                )
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                permissionsToRequest.add(Manifest.permission.ACTIVITY_RECOGNITION)
+            }
+        }
+
+        // POST_NOTIFICATIONS 권한 (Android 13 이상)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.POST_NOTIFICATIONS
+                )
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                permissionsToRequest.add(Manifest.permission.POST_NOTIFICATIONS)            }
+        }
+
+        if (permissionsToRequest.isNotEmpty()){
+            permissionLauncher.launch(permissionsToRequest.toTypedArray())
         }
     }
 }
