d259609fdc94b06ffffd0cce4a9c89d9c641d436
Jeonghoon Park <hoondb@naver.com>
2025-02-14 02:15:25 +0900

    feat: 메인화면 디자인 구조 변경

--- app/src/main/java/com/example/stepcounterapp/features/main/presentation/screen/MainScreen.kt
+++ app/src/main/java/com/example/stepcounterapp/features/main/presentation/screen/MainScreen.kt
@@ -3,15 +3,14 @@ package com.example.stepcounterapp.features.main.presentation.screen
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Divider
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import com.example.stepcounterapp.R
 import com.example.stepcounterapp.features.common.model.StepRecord
 import com.example.stepcounterapp.features.main.presentation.input.IMainViewModelInput
@@ -21,8 +20,7 @@ import com.example.stepcounterapp.features.main.presentation.screen.components.P
 import com.example.stepcounterapp.features.main.presentation.screen.components.RecordDetailSection
 import com.example.stepcounterapp.features.main.presentation.screen.components.SensorButtonSection
 import com.example.stepcounterapp.features.main.presentation.screen.components.StepCountSection
-import com.example.stepcounterapp.ui.components.button.SecondaryButton
-import com.example.stepcounterapp.ui.theme.colors
+import com.example.stepcounterapp.ui.components.button.CustomIconButton
 import java.time.LocalDateTime
 
 @Composable
@@ -40,10 +38,14 @@ fun MainScreen(
             MainTopAppBar(
                 date = currentDate,
                 button = {
-                    SecondaryButton(
-                        text = stringResource(id = R.string.view_records),
-                        modifier = Modifier.wrapContentSize(),
-                        onClick = { input.openSensitivityDialog() }
+                    CustomIconButton(
+                        onClick = { input.openSensitivityDialog() },
+                        icon = {
+                            Icon(
+                                imageVector = ImageVector.vectorResource(R.drawable.ic_chart),
+                                contentDescription = "정보 아이콘"
+                            )
+                        }
                     )
                 }
             )
@@ -54,19 +56,16 @@ fun MainScreen(
                 .fillMaxSize()
                 .padding(paddingValues)
         ) {
+            RecordDetailSection(
+                mainStateHolder = mainStateHolder,
+                stepRecord = stepRecord.value
+            )
             StepCountSection(
                 stepRecord = stepRecord.value,
                 modifier = Modifier.weight(1f)
             )
 
             SensorButtonSection()
-            Divider(color = MaterialTheme.colors.divider)
-
-            RecordDetailSection(
-                mainStateHolder = mainStateHolder,
-                stepRecord = stepRecord.value
-            )
-            Divider(color = MaterialTheme.colors.divider)
 
             PrimaryButtonSection(
                 mainStateHolder = mainStateHolder,
