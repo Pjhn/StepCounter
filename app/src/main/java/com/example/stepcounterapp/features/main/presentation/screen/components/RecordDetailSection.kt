906944d6293c43a39ac3ae30c8725d1f743028df
Jeonghoon Park <hoondb@naver.com>
2025-02-14 01:54:01 +0900

    feat: 활성화 아이콘 변경 및 디자인 수정
    - 패딩 변수 분리
    - 박스형 디자인으로 변경

--- app/src/main/java/com/example/stepcounterapp/features/main/presentation/screen/components/RecordDetailSection.kt
+++ app/src/main/java/com/example/stepcounterapp/features/main/presentation/screen/components/RecordDetailSection.kt
@@ -1,12 +1,15 @@
 package com.example.stepcounterapp.features.main.presentation.screen.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -14,7 +17,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
@@ -25,26 +31,38 @@ import com.example.stepcounterapp.ui.theme.Paddings
 import com.example.stepcounterapp.ui.theme.colors
 import kotlin.time.Duration
 
+private val SECTION_PADDING_HORIZONTAL = Paddings.extra
+private val CONTENT_PADDING = Paddings.medium
+private val CONTENT_BOX_SHAPE = 12.dp
+private val ICON_BOX_SIZE = 36.dp
+private val ICON_BOX_SHAPE = 6.dp
+
 @Composable
 fun RecordDetailSection(
     mainStateHolder: State<MainState>,
     stepRecord: StepRecord
 ) {
-    Box(
+    Row(
         modifier = Modifier
             .fillMaxWidth()
-            .height(56.dp)
+            .wrapContentHeight()
+            .padding(horizontal = SECTION_PADDING_HORIZONTAL),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
     ) {
-        Row(
+        Box(
             modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.Center)
-                .padding(start = Paddings.extra, end = Paddings.extra),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
+                .clip(RoundedCornerShape(CONTENT_BOX_SHAPE))
+                .background(MaterialTheme.colors.surface)
+                .padding(CONTENT_PADDING)
         ) {
-            CaloriesRow(mainStateHolder, stepRecord)
-            TimeRow(mainStateHolder, stepRecord)
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                CaloriesRow(mainStateHolder, stepRecord)
+                TimeRow(mainStateHolder, stepRecord)
+            }
         }
     }
 }
@@ -59,11 +77,20 @@ private fun CaloriesRow(
     Row(
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Icon(
-            imageVector = ImageVector.vectorResource(id = R.drawable.ic_cal),
-            tint = if (isMeasuring) MaterialTheme.colors.brandColor else MaterialTheme.colors.primary,
-            contentDescription = if (isMeasuring) "활성화 칼로리 아이콘" else "칼로리 아이콘",
-        )
+        Box(
+            modifier = Modifier
+                .size(ICON_BOX_SIZE)
+                .clip(RoundedCornerShape(ICON_BOX_SHAPE))
+                .background(MaterialTheme.colors.background),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = if (isMeasuring) painterResource(id = R.drawable.ic_cal_active) else
+                    painterResource(id = R.drawable.ic_cal),
+                contentDescription = if (isMeasuring) "Active Calories Icon" else "Calories Icon",
+                tint = Color.Unspecified
+            )
+        }
         Spacer(modifier = Modifier.padding(Paddings.small))
         Text(
             text = stepRecord.calories.toString(),
@@ -89,17 +116,27 @@ private fun TimeRow(
     Row(
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Icon(
-            imageVector = ImageVector.vectorResource(id = R.drawable.ic_time),
-            contentDescription = if (isMeasuring) "활성화 시계 아이콘" else "시계 아이콘",
-            tint = if (isMeasuring) MaterialTheme.colors.brandColor else MaterialTheme.colors.primary
-        )
+        Box(
+            modifier = Modifier
+                .size(ICON_BOX_SIZE)
+                .clip(RoundedCornerShape(ICON_BOX_SHAPE))
+                .background(MaterialTheme.colors.background),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                imageVector = if (isMeasuring) ImageVector.vectorResource(id = R.drawable.ic_time_active) else
+                    ImageVector.vectorResource(id = R.drawable.ic_time),
+                contentDescription = if (isMeasuring) "Active Time Icon" else "Time Icon",
+                tint = Color.Unspecified
+            )
+        }
         Spacer(modifier = Modifier.padding(Paddings.small))
         Text(
             text = formatDuration(stepRecord.measurementTime),
             style = MaterialTheme.typography.titleMedium,
             color = MaterialTheme.colorScheme.outline
         )
+        Spacer(modifier = Modifier.padding(Paddings.small))
     }
 }
 
