26bb4fec19c8bf378da91a002747d6a53bebc4d0
Jeonghoon Park <hoondb@naver.com>
2025-02-14 01:55:55 +0900

    feat: 테두리 형식 버튼으로 변경

--- app/src/main/java/com/example/stepcounterapp/ui/components/button/SecondaryButton.kt
+++ app/src/main/java/com/example/stepcounterapp/ui/components/button/SecondaryButton.kt
@@ -1,24 +1,27 @@
 package com.example.stepcounterapp.ui.components.button
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.stepcounterapp.ui.theme.Paddings
 import com.example.stepcounterapp.ui.theme.StepCounterAppTheme
+import com.example.stepcounterapp.ui.theme.colors
 
 private val SECONDARY_BUTTON_PADDING_VERTICAL = Paddings.medium
-private val SECONDARY_BUTTON_PADDING_HORIZONTAL = Paddings.large
+private val SECONDARY_BUTTON_PADDING_HORIZONTAL = Paddings.small
+private val BUTTON_BORDER_WIDTH = 1.dp
 
 @Composable
 fun SecondaryButton(
@@ -26,28 +29,24 @@ fun SecondaryButton(
     @StringRes id: Int? = null,
     text: String = "",
     onClick: () -> Unit
-){
-    Button(
-        modifier = modifier.wrapContentSize()
+) {
+    OutlinedButton(
+        modifier = modifier
+            .wrapContentSize()
             .padding(
                 vertical = SECONDARY_BUTTON_PADDING_VERTICAL,
                 horizontal = SECONDARY_BUTTON_PADDING_HORIZONTAL
             ),
         shape = MaterialTheme.shapes.medium,
         onClick = onClick,
-        colors = ButtonDefaults.buttonColors(
-            containerColor = MaterialTheme.colorScheme.secondary,
-            contentColor = MaterialTheme.colorScheme.outline,
-            disabledContentColor = MaterialTheme.colorScheme.secondary,
-            disabledContainerColor = MaterialTheme.colorScheme.outline
-        )
-    ){
+        border = BorderStroke(BUTTON_BORDER_WIDTH, MaterialTheme.colors.outlinedButton)
+    ) {
         Row(
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically
         ) {
             Text(
-                text = id?.let { stringResource(id = id)} ?: text,
+                text = id?.let { stringResource(id = id) } ?: text,
                 style = MaterialTheme.typography.labelLarge,
             )
         }
@@ -56,7 +55,7 @@ fun SecondaryButton(
 
 @Preview
 @Composable
-fun SecondaryButtonPreview(){
+fun SecondaryButtonPreview() {
     StepCounterAppTheme {
         SecondaryButton(
             text = "PreView"
