85d934de4fb984d990d75de35de9f7aa0e6bb456
Jeonghoon Park <hoondb@naver.com>
2025-02-14 01:46:04 +0900

    refactor: 수평 패딩 삭제 및 가독성 개선

--- app/src/main/java/com/example/stepcounterapp/ui/components/button/PrimaryButton.kt
+++ app/src/main/java/com/example/stepcounterapp/ui/components/button/PrimaryButton.kt
@@ -1,25 +1,21 @@
 package com.example.stepcounterapp.ui.components.button
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.stepcounterapp.ui.theme.Paddings
 import com.example.stepcounterapp.ui.theme.StepCounterAppTheme
 
-private val PRIMARY_BUTTON_PADDING_VERTICAL = Paddings.small
-private val PRIMARY_BUTTON_PADDING_HORIZONTAL = 76.dp
+private val PRIMARY_BUTTON_PADDING_VERTICAL = Paddings.xlarge
 
 @Composable
 fun PrimaryButton(
@@ -30,24 +26,16 @@ fun PrimaryButton(
     colors: ButtonColors
 ) {
     Button(
-        modifier = modifier,
+        modifier = modifier.fillMaxWidth(),
         shape = MaterialTheme.shapes.large,
         onClick = onClick,
-        colors = colors
+        colors = colors,
+        contentPadding = PaddingValues(PRIMARY_BUTTON_PADDING_VERTICAL)
     ) {
-        Row(
-            modifier = modifier.padding(
-                vertical = PRIMARY_BUTTON_PADDING_VERTICAL,
-                horizontal = PRIMARY_BUTTON_PADDING_HORIZONTAL
-            ),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = id?.let { stringResource(id = id) } ?: text,
-                style = MaterialTheme.typography.titleMedium,
-            )
-        }
+        Text(
+            text = id?.let { stringResource(id = id) } ?: text,
+            style = MaterialTheme.typography.titleMedium,
+        )
     }
 }
 
@@ -56,7 +44,7 @@ fun PrimaryButton(
 fun PrimaryButtonPreview() {
     StepCounterAppTheme {
         PrimaryButton(
-            text = "미리보기",
+            text = "Preview",
             colors = ButtonDefaults.buttonColors(
                 containerColor = MaterialTheme.colorScheme.primary,
                 contentColor = MaterialTheme.colorScheme.onPrimary,
