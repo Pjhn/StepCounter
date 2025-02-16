32e4230fcaeb6f010fa2c2058ed53a24fb001b63
Jeonghoon Park <hoondb@naver.com>
2025-02-16 20:43:45 +0900

    chore: mpandroidchart 버전 관리

---
+++ app/src/main/java/com/example/stepcounterapp/features/record/presentation/screen/components/CategoryBar.kt
@@ -0,0 +1,74 @@
+package com.example.stepcounterapp.features.record.presentation.screen.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.stepcounterapp.features.record.domain.enums.RecordCategories
+import com.example.stepcounterapp.features.record.domain.enums.RecordCategories.*
+import com.example.stepcounterapp.ui.theme.colors
+
+@Composable
+fun CategoryBar(
+    selectedCategory: RecordCategories,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(24.dp,  Alignment.CenterHorizontally)
+    ) {
+        entries.forEach { category ->
+            CategoryBox(
+                category = category,
+                isSelected = category == selectedCategory,
+                onClick = { })
+        }
+    }
+}
+
+@Composable
+fun CategoryBox(
+    category: RecordCategories,
+    isSelected: Boolean,
+    onClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier.clickable(indication = null, interactionSource = remember {
+            MutableInteractionSource()
+        }) { onClick() },
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = category.toString(),
+            style = MaterialTheme.typography.titleMedium.copy(
+                fontWeight = if (isSelected) FontWeight.Bold else FontWeight.Normal
+            ),
+            color = if (isSelected) MaterialTheme.colors.text else MaterialTheme.colors.defaultTextButton
+        )
+        if (isSelected) {
+            Spacer(modifier = Modifier.height(4.dp))
+            Box(
+                modifier = Modifier
+                    .size(width = 20.dp, height = 2.dp)
+                    .background(MaterialTheme.colors.brandColor)
+            )
+        }
+    }
+}
