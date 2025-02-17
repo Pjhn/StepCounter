c3875148aa4bda19bec26db4f83e6adac7012acb
Jeonghoon Park <hoondb@naver.com>
2025-02-17 23:07:37 +0900

    feat: 엔티티 mapper 추가

---
+++ app/src/main/java/com/example/stepcounterapp/features/common/config/StepRecordConfig.kt
@@ -0,0 +1,7 @@
+package com.example.stepcounterapp.features.common.config
+
+object StepRecordConfig {
+    const val STEP_LENGTH = 0.8
+    const val CALORIE_PER_STEP = 0.04
+    const val TIME_PER_STEP = 0.6
+}
