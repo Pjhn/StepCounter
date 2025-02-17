33cfd76f5de8af557ccd8897674e7fc6b0202b6d
Jeonghoon Park <hoondb@naver.com>
2025-02-17 23:08:18 +0900

    refactor: 기록 불러오기 매서드 구현 및 mapper 적용

--- app/src/main/java/com/example/stepcounterapp/features/common/repository/UserRecordRepository.kt
+++ app/src/main/java/com/example/stepcounterapp/features/common/repository/UserRecordRepository.kt
@@ -2,40 +2,26 @@ package com.example.stepcounterapp.features.common.repository
 
 import com.example.stepcounterapp.features.common.database.dao.StepRecordDao
 import com.example.stepcounterapp.features.common.database.entity.StepRecordEntity
+import com.example.stepcounterapp.features.common.database.entity.mapper.toDomain
 import com.example.stepcounterapp.features.common.model.StepRecord
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.map
 import java.time.LocalDate
 import javax.inject.Inject
-import kotlin.time.Duration.Companion.seconds
 
 class UserRecordRepository @Inject constructor(
     private val stepRecordDao: StepRecordDao
 ) : IUserRecordRepository {
 
     override val userRecord = stepRecordDao.getLatestStepRecord().map { entity ->
-        entity?.let {
-            val date = it.date
-            val stepCount = it.stepCount
-            val distance = it.stepCount * STEP_LENGTH
-            val calories = it.stepCount * CALORIE_PER_STEP
-            val measurementTime = (it.stepCount * TIME_PER_STEP).seconds
-
-            StepRecord(
-                date = date,
-                stepCount = stepCount,
-                distance = distance,
-                calories = calories,
-                measurementTime = measurementTime
-            )
-        } ?: StepRecord()
+        entity?.toDomain() ?: StepRecord()
     }
 
     override suspend fun initializeTodayRecord() {
         val now = LocalDate.now()
         val latestEntity = stepRecordDao.getLatestStepRecord().firstOrNull()
 
-        if (latestEntity == null || latestEntity.date < now){
+        if (latestEntity == null || latestEntity.date < now) {
             val entity = StepRecordEntity(
                 date = now,
                 stepCount = 0
@@ -53,9 +39,10 @@ class UserRecordRepository @Inject constructor(
         stepRecordDao.upsert(entity)
     }
 
-    companion object {
-        private const val STEP_LENGTH = 0.8
-        private const val CALORIE_PER_STEP = 0.04
-        private const val TIME_PER_STEP = 0.6
+    override suspend fun getRecordsForPeriod(
+        startDate: LocalDate,
+        endDate: LocalDate
+    ): List<StepRecord> {
+        return stepRecordDao.getStepRecords(startDate, endDate).toDomain()
     }
 }
