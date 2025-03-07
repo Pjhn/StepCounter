86484837c8cadda2d392313f5f33571ad14fd876
Jeonghoon Park <hoondb@naver.com>
2025-03-07 18:59:40 +0900

    feat: Glance 위젯 리시버 추가

---
+++ app/src/main/java/com/example/stepcounterapp/features/widget/StepCountWidgetReceiver.kt
@@ -0,0 +1,9 @@
+package com.example.stepcounterapp.features.widget
+
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class StepCountWidgetReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget
+        get() = StepCountWidget
+}
