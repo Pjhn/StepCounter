b59bb0c251dca5b2edc57d7c2bdb6e9fb1f325a3
Jeonghoon Park <hoondb@naver.com>
2025-03-14 15:41:32 +0900

    design: 배경색상 추가

--- app/src/main/java/com/example/stepcounterapp/features/widget/StepCountWidget.kt
+++ app/src/main/java/com/example/stepcounterapp/features/widget/StepCountWidget.kt
@@ -1,6 +1,8 @@
 package com.example.stepcounterapp.features.widget
 
 import android.content.Context
+import android.util.Log
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -8,11 +10,13 @@ import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
+import androidx.glance.background
 import androidx.glance.layout.Column
 import androidx.glance.layout.padding
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
 import com.example.stepcounterapp.features.common.repository.IUserRecordRepository
+import com.example.stepcounterapp.ui.theme.colors
 import dagger.hilt.android.EntryPointAccessors
 import kotlinx.coroutines.flow.first
 
@@ -32,7 +36,10 @@ object StepCountWidget : GlanceAppWidget() {
 
     @Composable
     private fun Content(stepCount: Int){
-        Column(modifier = GlanceModifier.padding(16.dp)) {
+        Column(
+            modifier = GlanceModifier.padding(16.dp)
+                .background(MaterialTheme.colors.background),
+        ) {
             Text(text = "Steps: $stepCount", style = TextStyle(fontSize = 18.sp))
         }
     }
