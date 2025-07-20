bea11d8361e93ac298382342963f055ce48b3d3e
Jeonghoon Park <hoondb@naver.com>
2025-07-21 00:12:02 +0900

    fix: 위젯 갱신 버그 수정 - glance preferences 사용 - 서비스 로직 수정

--- app/src/main/java/com/example/stepcounterapp/features/widget/StepCountWidget.kt
+++ app/src/main/java/com/example/stepcounterapp/features/widget/StepCountWidget.kt
@@ -1,46 +1,45 @@
-package com.example.stepcounterapp.features.widget
-
 import android.content.Context
-import android.util.Log
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
+import androidx.glance.currentState
 import androidx.glance.layout.Column
 import androidx.glance.layout.padding
+import androidx.glance.state.PreferencesGlanceStateDefinition
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
-import com.example.stepcounterapp.features.common.repository.IUserRecordRepository
-import com.example.stepcounterapp.ui.theme.colors
-import dagger.hilt.android.EntryPointAccessors
-import kotlinx.coroutines.flow.first
 
 object StepCountWidget : GlanceAppWidget() {
-    override suspend fun provideGlance(context: Context, id: GlanceId) {
-        val entryPoint = EntryPointAccessors.fromApplication(
-            context.applicationContext,
-            WidgetRepositoryEntryPoint::class.java
-        )
-        val repository: IUserRecordRepository = entryPoint.getUserRecordRepository()
-        val stepCount = repository.userRecord.first().stepCount ?: 0
 
+    override val stateDefinition = PreferencesGlanceStateDefinition
+    val TODAY_STEPS = intPreferencesKey("today_steps")
+
+    override suspend fun provideGlance(context: Context, id: GlanceId) {
         provideContent {
-            Content(stepCount)
+            Content()
         }
     }
 
     @Composable
-    private fun Content(stepCount: Int){
+    fun Content() {
+        val prefs = currentState<Preferences>()
+        val steps = prefs[TODAY_STEPS] ?: 0
+
         Column(
-            modifier = GlanceModifier.padding(16.dp)
-                .background(MaterialTheme.colors.background),
+            GlanceModifier
+                .padding(16.dp)
+                .background(MaterialTheme.colorScheme.background)
         ) {
-            Text(text = "Steps: $stepCount", style = TextStyle(fontSize = 18.sp))
+            Text("STEPS", style = TextStyle(fontSize = 14.sp))
+            Text("$steps", style = TextStyle(fontSize = 26.sp))
         }
     }
 }
