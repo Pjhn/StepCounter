bfaf8b0416b0328a698098f05bbbeb3c81efad19
Jeonghoon Park <hoondb@naver.com>
2025-08-16 08:34:45 +0900

    feat: modal navigation 추가

--- app/src/main/java/com/example/stepcounterapp/features/main/presentation/screen/MainScreen.kt
+++ app/src/main/java/com/example/stepcounterapp/features/main/presentation/screen/MainScreen.kt
@@ -3,14 +3,26 @@ package com.example.stepcounterapp.features.main.presentation.screen
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
 import com.example.stepcounterapp.R
 import com.example.stepcounterapp.features.common.model.StepRecord
 import com.example.stepcounterapp.features.main.presentation.input.IMainViewModelInput
@@ -22,7 +34,12 @@ import com.example.stepcounterapp.features.main.presentation.screen.components.R
 import com.example.stepcounterapp.features.main.presentation.screen.components.SensorButtonSection
 import com.example.stepcounterapp.features.main.presentation.screen.components.StepCountSection
 import com.example.stepcounterapp.ui.components.button.CustomIconButton
+import kotlinx.coroutines.launch
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+
+private const val DATE_FMT_US = "MM/dd/yyyy"
 
 @Composable
 fun MainScreen(
@@ -31,52 +48,53 @@ fun MainScreen(
     input: IMainViewModelInput,
     stepRecord: State<StepRecord>
 ) {
-    val currentDate = remember {
-        LocalDateTime.now()
-    }
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
 
-    Scaffold(
-        topBar = {
-            MainTopAppBar(
-                date = currentDate,
-                button = {
-                    CustomIconButton(
-                        onClick = { input.openRecord() },
-                        icon = {
-                            Icon(
-                                imageVector = ImageVector.vectorResource(R.drawable.ic_chart),
-                                contentDescription = "record"
-                            )
-                        }
-                    )
-                }
-            )
-        }
-    ) { paddingValues ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-        ) {
-            RecordDetailSection(
-                mainStateHolder = mainStateHolder,
-                stepRecord = stepRecord.value
-            )
+    ModalNavigationDrawer(
+        drawerContent = {
+            ModalDrawerSheet {
+            Text("Drawer title", modifier = Modifier.padding(16.dp))
+            }
+        },
+        drawerState = drawerState
+    ) {
+        Scaffold(
+            topBar = {
+                MainTopAppBar(
+                    title = LocalDateTime.now()
+                        .format(DateTimeFormatter.ofPattern(DATE_FMT_US)),
+                    input = input,
+                    scope = scope,
+                    drawerState = drawerState
+                )
+            }
+        ) { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+            ) {
+                RecordDetailSection(
+                    mainStateHolder = mainStateHolder,
+                    stepRecord = stepRecord.value
+                )
 
-            StepCountSection(
-                stepRecord = stepRecord.value,
-                modifier = Modifier.weight(1f)
-            )
+                StepCountSection(
+                    stepRecord = stepRecord.value,
+                    modifier = Modifier.weight(1f)
+                )
 
-            SensorButtonSection(
-                sensorStateHolder = sensorStateHolder,
-                input = input
-            )
+                SensorButtonSection(
+                    sensorStateHolder = sensorStateHolder,
+                    input = input
+                )
 
-            PrimaryButtonSection(
-                mainStateHolder = mainStateHolder,
-                input = input
-            )
+                PrimaryButtonSection(
+                    mainStateHolder = mainStateHolder,
+                    input = input
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/example/stepcounterapp/features/main/presentation/screen/components/MainTopAppBar.kt
+++ app/src/main/java/com/example/stepcounterapp/features/main/presentation/screen/components/MainTopAppBar.kt
@@ -2,13 +2,22 @@ package com.example.stepcounterapp.features.main.presentation.screen.components
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerState
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.example.stepcounterapp.R
+import com.example.stepcounterapp.features.main.presentation.input.IMainViewModelInput
+import com.example.stepcounterapp.ui.components.button.CustomIconButton
 import com.example.stepcounterapp.ui.theme.Paddings
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.Locale
@@ -19,8 +28,10 @@ private val TOP_APP_BAR_HORIZONTAL = Paddings.xxlarge
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainTopAppBar(
-    date: LocalDateTime,
-    button: @Composable (() -> Unit) = {}
+    title: String,
+    input: IMainViewModelInput,
+    scope: CoroutineScope,
+    drawerState: DrawerState
 ) {
     CenterAlignedTopAppBar(
         modifier = Modifier.padding(
@@ -29,14 +40,39 @@ fun MainTopAppBar(
         ),
         title = {
             Text(
-                text = date.format(
-                    DateTimeFormatter.ofPattern("MM/dd/yyyy", Locale.US)
-                ),
+                text = title,
                 style = MaterialTheme.typography.titleMedium
             )
         },
+        navigationIcon = {
+            CustomIconButton(
+                icon = {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.ic_modal),
+                        contentDescription = "settings"
+                    )
+                },
+                onClick = {
+                    scope.launch {
+                        if (drawerState.isClosed) {
+                            drawerState.open()
+                        } else {
+                            drawerState.close()
+                        }
+                    }
+                }
+            )
+        },
         actions = {
-            button()
+            CustomIconButton(
+                onClick = { input.openRecord() },
+                icon = {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.ic_chart),
+                        contentDescription = "record"
+                    )
+                }
+            )
         },
         colors = TopAppBarDefaults.topAppBarColors(MaterialTheme.colorScheme.background),
     )
