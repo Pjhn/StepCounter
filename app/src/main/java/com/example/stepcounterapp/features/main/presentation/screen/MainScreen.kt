91d93de17fb90829b8dde376ee5b8f9d72bbd903
Jeonghoon Park <hoondb@naver.com>
2025-08-16 13:13:31 +0900

    feat: modal navigation 컨텐츠 추가
    - 걸음 센서 딜레이 변경
    - 위젯 추가

--- app/src/main/java/com/example/stepcounterapp/features/main/presentation/screen/MainScreen.kt
+++ app/src/main/java/com/example/stepcounterapp/features/main/presentation/screen/MainScreen.kt
@@ -2,11 +2,17 @@ package com.example.stepcounterapp.features.main.presentation.screen
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
@@ -14,17 +20,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.stepcounterapp.R
 import com.example.stepcounterapp.features.common.model.StepRecord
 import com.example.stepcounterapp.features.main.presentation.input.IMainViewModelInput
 import com.example.stepcounterapp.features.main.presentation.output.MainState
 import com.example.stepcounterapp.features.main.presentation.output.SensorState
+import com.example.stepcounterapp.features.main.presentation.screen.components.MainModalItem
 import com.example.stepcounterapp.features.main.presentation.screen.components.MainTopAppBar
 import com.example.stepcounterapp.features.main.presentation.screen.components.PrimaryButtonSection
 import com.example.stepcounterapp.features.main.presentation.screen.components.RecordDetailSection
 import com.example.stepcounterapp.features.main.presentation.screen.components.SensorButtonSection
 import com.example.stepcounterapp.features.main.presentation.screen.components.StepCountSection
+import com.example.stepcounterapp.ui.components.button.SecondaryButton
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -43,7 +52,10 @@ fun MainScreen(
     ModalNavigationDrawer(
         drawerContent = {
             ModalDrawerSheet {
-            Text("Drawer title", modifier = Modifier.padding(16.dp))
+                ModalContent(
+                    sensorState = sensorStateHolder.value,
+                    input = input
+                )
             }
         },
         drawerState = drawerState
@@ -101,6 +113,44 @@ fun MainContent(
         PrimaryButtonSection(
             mainState = mainState,
             input = input
+        )
+    }
+}
+
+@Composable
+fun ModalContent(
+    sensorState: SensorState,
+    input: IMainViewModelInput
+){
+    Column(
+        modifier = Modifier.padding(horizontal = 16.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        Spacer(Modifier.height(12.dp))
+
+        Text("Settings", modifier = Modifier.padding(16.dp))
+
+        NavigationDrawerItem(
+            label = { Text("Add Widget") },
+            selected = false,
+            onClick = { input.requestWidget() }
+        )
+
+        Divider()
+
+        MainModalItem(
+            id = R.string.sensor_delay,
+            button = {
+                SecondaryButton(
+                    text = stringResource(
+                        id = when (sensorState) {
+                            SensorState.DelayHigh -> R.string.high
+                            SensorState.DelayLow -> R.string.low
+                        }
+                    ),
+                    onClick = { input.updateSensorDelay() }
+                )
+            }
         )
     }
 }
---
+++ app/src/main/java/com/example/stepcounterapp/features/main/presentation/screen/components/MainModalItem.kt
@@ -0,0 +1,36 @@
+package com.example.stepcounterapp.features.main.presentation.screen.components
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.stepcounterapp.ui.theme.Paddings
+
+@Composable
+fun MainModalItem(
+    @StringRes id: Int? = null,
+    text: String = "",
+    button: @Composable () -> Unit?
+){
+    Row(
+        modifier = Modifier.padding(start = 16.dp, end = 24.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = id?.let{ stringResource(id) } ?: text,
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.outline,
+        )
+        Spacer(Modifier.padding(horizontal = Paddings.xxlarge))
+        button()
+    }
+}
