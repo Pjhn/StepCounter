10ee49475231aa2b9a72470db6e117f5880c5a3a
Jeonghoon Park <hoondb@naver.com>
2025-09-21 00:40:46 +0900

    chore: 버전 변경
    - 1.0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.pjhn.stepcounter"
         minSdk = 28
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.0.6"
+        versionCode = 8
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
